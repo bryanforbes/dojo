5a1698c00261a8a355c0fada6fafc1cbeb01e3d3
Nick Nisi <nick@nisi.org>
2014-01-01 23:56:42 -0600

    Clean up _base/json tests
    + replace many assertions with a single deepEqual
    + wrap code that is supposed to throw an error in assert.throws
    + clean up test names

--- tests-intern/unit/_base/json.js
+++ tests-intern/unit/_base/json.js
@@ -5,7 +5,7 @@ define([
 ], function (registerSuite, assert, json) {
 	registerSuite({
 		name: 'dojo/_base/json',
-		'toAndFromJson': function () {
+		'toJson and fromJson': function () {
 			// Not testing dojo.toJson() on its own since Rhino will output the object properties in a different order.
 			// Still valid json, but just in a different order than the source string.
 
@@ -28,29 +28,17 @@ define([
 			},
 			obj = json.fromJson(json.toJson(testObj));
 
-			assert.equal('a', obj.a);
-			assert.equal(1, obj.b);
-			assert.equal('c', obj.c);
-			assert.equal('d', obj.d);
-			assert.equal('e1', obj.e.e1);
-			assert.equal(2, obj.e.e2);
-			assert.equal(1, obj.f[0]);
-			assert.equal(2, obj.f[1]);
-			assert.equal(3, obj.f[2]);
-			assert.equal('g', obj.g);
-			assert.equal('h3', obj.h.h1.h2.h3);
-			assert.lengthOf(obj.i[0], 3);
-			assert.lengthOf(obj.i[1], 1);
-			assert.lengthOf(obj.i[2], 1);
+			assert.deepEqual(obj, testObj);
 
 			var badJson;
-			try {
+			assert.throws(function () {
 				badJson = json.fromJson('bad json'); // should throw an exception, and not set badJson
-			} catch (e) {}
+			});
+
 			assert.isUndefined(badJson);
 		},
 
-		'dojoExtendedJson': function () {
+		'dojo extended json': function () {
 			var testObj = {
 				ex1: {
 					b: 3,
@@ -69,7 +57,7 @@ define([
 			assert.equal('{"ex1":"json3","ex2":"__json__4"}', testStr);
 		},
 
-		'prettyPrintJson': function () {
+		'pretty print json': function () {
 			if (typeof JSON === 'undefined') { // only test our JSON stringifier
 				var testObj = {array: [1, 2, { a: 4, b: 4 }]};
 				var testStr = json.toJson(testObj, true);
@@ -77,7 +65,7 @@ define([
 			}
 		},
 
-		'evalJson': function () {
+		'eval json': function () {
 			var testStr = '{func: function(){}, number: Infinity}';
 			var testObj = json.fromJson(testStr);
 
@@ -85,7 +73,7 @@ define([
 			assert.isNumber(testObj.number);
 		},
 
-		'toJsonStringObject': function () {
+		'toJson called on string': function () {
 			assert.equal('"hello"', json.toJson('hello'));
 		}
 	});
