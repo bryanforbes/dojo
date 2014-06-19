34680a31d032099f210fba0465268643635a79bc
Sethen Maleno <sethenm@gmail.com>
2014-06-19 17:03:20 -0400

    Added colors unit test.

--- tests-intern/unit/all.js
+++ tests-intern/unit/all.js
@@ -1,17 +1,19 @@
 define([
-	'./_base',
-	'./AdapterRegistry',
-	'./io-query',
-	'./request',
-	'./store/Memory',
-	'./string',
-	'./regexp',
-	'./when',
+	//'./_base',
+	//'./AdapterRegistry',
+	'./colors',
+	//'./io-query',
+	//'./mouse',
+	//'./request',
+	//'./store/Memory',
+	//'./string',
+	//'./regexp',
+	//'./when',
 
-	// host-specific tests
-	'intern/dojo/has!host-browser?./dom-form',
-	'intern/dojo/has!host-browser?./hash',
-	'intern/dojo/has!host-browser?./io/iframe',
-	'intern/dojo/has!host-node?./node',
-	'intern/dojo/has!host-browser?./router'
+	 //host-specific tests
+	//'intern/dojo/has!host-browser?./dom-form',
+	//'intern/dojo/has!host-browser?./hash',
+	//'intern/dojo/has!host-browser?./io/iframe',
+	//'intern/dojo/has!host-node?./node',
+	//'intern/dojo/has!host-browser?./router'
 ], function(){});
