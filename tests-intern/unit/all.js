744d3321b224d7d340cec4c082d6574477efd7d3
Sethen Maleno <sethenm@gmail.com>
2014-06-19 17:08:58 -0400

    Adding in other unit tests.

--- tests-intern/unit/all.js
+++ tests-intern/unit/all.js
@@ -1,19 +1,18 @@
 define([
-	//'./_base',
-	//'./AdapterRegistry',
+	'./_base',
+	'./AdapterRegistry',
 	'./colors',
-	//'./io-query',
-	//'./mouse',
-	//'./request',
-	//'./store/Memory',
-	//'./string',
-	//'./regexp',
-	//'./when',
+	'./io-query',
+	'./request',
+	'./store/Memory',
+	'./string',
+	'./regexp',
+	'./when',
 
 	 //host-specific tests
-	//'intern/dojo/has!host-browser?./dom-form',
-	//'intern/dojo/has!host-browser?./hash',
-	//'intern/dojo/has!host-browser?./io/iframe',
-	//'intern/dojo/has!host-node?./node',
-	//'intern/dojo/has!host-browser?./router'
+	'intern/dojo/has!host-browser?./dom-form',
+	'intern/dojo/has!host-browser?./hash',
+	'intern/dojo/has!host-browser?./io/iframe',
+	'intern/dojo/has!host-node?./node',
+	'intern/dojo/has!host-browser?./router'
 ], function(){});
