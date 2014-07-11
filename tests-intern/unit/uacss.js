c1c64bbc6523c5c63019d20c4334b5050ea1dd53
bitpshr <paulbouchon90@gmail.com>
2014-07-11 15:14:54 -0400

    uacss tests

---
+++ tests-intern/unit/uacss.js
@@ -0,0 +1,4 @@
+define([
+	'intern/dojo/has!host-browser?./uacss/sniffQuirks',
+	'intern/dojo/has!host-browser?./uacss/sniffStandards'
+], function () {});
---
+++ tests-intern/unit/uacss/sniffQuirks.js
@@ -0,0 +1,40 @@
+define([
+	'require',
+	'intern!object',
+	'intern/chai!assert',
+	'dojo/Deferred',
+	'dojo/_base/lang',
+	'dojo/parser',
+	'dojo/dom',
+	'dojo/json',
+	'dojo/has',
+	'dojo/dom-geometry',
+	'dojo/dom-style'
+], function(require, registerSuite, assert, Deferred, lang, parser, dom, json, has, domGeom, domStyle) {
+	registerSuite({
+		name: 'dojo/uacss sniff quirks',
+
+		before: function () {
+			var deferred = new Deferred();
+			require(['dojo/text!./sniffQuirks.html'], function(html) {
+				document.body.innerHTML = html;
+				parser.parse().then(deferred.resolve, deferred.reject);
+			});
+			return deferred.promise;
+		},
+
+		'measure node': function () {
+			var node = dom.byId('box1');
+			var reportNode = dom.byId('log');
+			reportNode.innerHTML = json.stringify({
+				boxModel: domGeom.boxModel,
+				hasQuirks: has('quirks'),
+				width: domStyle.get(node, 'width'),
+				marginBoxWidth: domGeom.getMarginBox(node).w,
+				htmlClassName: document.documentElement.className
+			}, null, '    ');
+			
+			assert.strictEqual(node.offsetWidth, 100);
+		}
+	});
+});
