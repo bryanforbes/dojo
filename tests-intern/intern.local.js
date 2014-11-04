97058fb9a2cd3bd398707b8cce07a5d17f0231c2
Jason Cheatham <j.cheatham@gmail.com>
2014-11-04 15:32:53 -0500

    Improve the local intern test config

--- tests-intern/intern.local.js
+++ tests-intern/intern.local.js
@@ -1,16 +1,39 @@
 define([
-	'./intern'
-], function (intern) {
-	intern.tunnel = 'NullTunnel';
-	intern.tunnelOptions = {
-		hostname: 'localhost',
-		port: 4444
+	'./intern',
+	'intern'
+], function (config, intern) {
+	var args = intern.args;
+
+	config.tunnel = args.tunnel || 'NullTunnel';
+
+	config.tunnelOptions = {
+		hostname: args.tunnelHost || 'localhost',
+		port: Number(args.tunnelPort) || 4444
 	};
 
-	intern.environments = [
-		{ browserName: 'firefox' },
-		{ browserName: 'chrome' }
-	];
+	var proxyHost = args.proxyHost || 'localhost';
+	config.proxyPort = Number(args.proxyPort) || 9001;
+	config.proxyUrl = 'http://' + proxyHost + ':' + config.proxyPort + '/';
 
-	return intern;
+	switch (args.env) {
+		case 'ff':
+			config.environments = [ { browserName: 'firefox' } ];
+			break;
+		case 'chrome':
+			config.environments = [ { browserName: 'chrome', chromeOptions: { args: [ '--test-type' ] } } ];
+			break;
+		case 'safari':
+			config.environments = [ { browserName: 'safari' } ];
+			break;
+		case 'ie':
+			config.environments = [ { browserName: 'internet explorer' } ];
+			break;
+		default:
+			config.environments = [
+				{ browserName: 'firefox' },
+				{ browserName: 'chrome', chromeOptions: { args: [ '--test-type' ] } }
+			];
+	}
+
+	return config;
 });
